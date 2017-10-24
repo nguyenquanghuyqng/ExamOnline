c08078ce3e8eadf7a7c146f0025004b472e1f5bf
Trung <Trungspt15@gmail.com>
2017-10-24 22:37:48 +0700

    Them Bean.AnswerUser

---
+++ ExamCNPM/src/BEAN/AnswerUser.java
@@ -0,0 +1,18 @@
+package BEAN;
+
+public class AnswerUser {
+	private int number;
+	private String answer;
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+}
