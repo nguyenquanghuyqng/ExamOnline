6eecab41de52fbdc0484f2ea391192fcdb5b2fcc
Trung <Trungspkt15@gmail.com>
2017-11-07 11:09:26 +0700

    Update student profile 2

--- ExamCNPM/src/Controller/ViewProfile.java
+++ ExamCNPM/src/Controller/ViewProfile.java
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-package Controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import BEAN.User;
-import DAO.LoginDAO;
-
-@WebServlet("/ViewProfile")
-public class ViewProfile extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	public ViewProfile() {
-		super();
-	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doPost(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("View/ViewProfile.jsp");
-		rd.forward(request, response);
-	}
-
-}
-=======
 package Controller;
 
 import java.io.IOException;
@@ -89,4 +53,4 @@ public class ViewProfile extends HttpServlet {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/nguyenquanghuyqng/ExamOnline.git
+
