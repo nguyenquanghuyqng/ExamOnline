ba69d26558be9339da1bbff5dcbd8f260bb3277f
Nguyễn Quang Huy <nguyenquanghuy605@gmail.com>
2017-11-14 13:41:57 +0700

    Huy

--- ExamCNPM/src/Controller/EditProfile.java
+++ ExamCNPM/src/Controller/EditProfile.java
@@ -19,8 +19,7 @@ public class EditProfile extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
-		
+		doPost(request, response);	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
